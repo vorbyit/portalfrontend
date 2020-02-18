ef2016aa9889d5860eb8e1af4a5d1babc5e6a926
manish0870 <manishchoudhary0870@gmail.com>
2020-02-18 23:21:51 +0530

    signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,86 +1,106 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import API from '../API';
+import '../css/register.css';
 
 export default class SignupForm extends Component {
-	constructor() {
-		super()
-		this.state = {
-			name : '',
-			email : '',
-			username : '',
-			password : '',
-			cnfpassword : '',
-			mobile : '',
-		}
-		this.handleChange=this.handleChange.bind(this);
-		this.handleSubmit=this.handleSubmit.bind(this);	
-	}
-	
-	handleChange(evt) {
-    this.setState({ [evt.target.name]: evt.target.value });
-	}
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      username: '',
+      password: '',
+      cnfpassword: '',
+      mobile: '',
+      sex: 'male'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-	async handleSubmit(evt) {
-		evt.preventDefault();
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  async handleSubmit(evt) {
+    evt.preventDefault();
     try {
       const newUser = this.state;
-	  const { data } = await API.post('auth/register', newUser);
+      const { data } = await API.post('auth/register', newUser);
       this.props.updateUser(data);
       this.props.history.push('/');
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-	}
+  }
 
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.handleSubmit}>
-					<input 
-						type="text" 
-						name="name" 
-						id="name"
-						value={this.state.name}
-						onChange={this.handleChange}
-					/>
-					<input 
-						type="email" 
-						name="email" 
-						id="email"
-						value={this.state.email}
-						onChange={this.handleChange}
-					/>
-					<input 
-						type="text" 
-						name="username" 
-						id="username"
-						value={this.state.username}
-						onChange={this.handleChange}
-					/>
-					<input 
-						type="password" 
-						name="password" 
-						id="password"
-						value={this.state.password}
-						onChange={this.handleChange}
-					/>
-					<input 
-						type="password" 
-						name="cnfpassword" 
-						id="cnfpassword"
-						value={this.state.cnfpassword}
-						onChange={this.handleChange}
-					/>
-					<input 
-						type="text" 
-						name="mobile" 
-						id="mobile"
-						value={this.state.mobile}
-						onChange={this.handleChange}
-					/>
-					<button type="submit">SUBMIT</button>
-				</form>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div id="signup-container">
+        <div className="form-wrap">
+          <h1>Create Account</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" id="name" value={this.state.name} onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" value={this.state.email} onChange={this.handleChange} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="cnfpassword">Conform Password</label>
+              <input
+                type="password"
+                name="cnfpassword"
+                id="cnfpassword"
+                value={this.state.cnfpassword}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label for="sex">Gender</label>
+              <select name="sex" id="sex" value={this.state.sex} onChange={this.handleChange}>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="others">Others</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Contact Number</label>
+              <input type="text" name="mobile" id="mobile" value={this.state.mobile} onChange={this.handleChange} />
+            </div>
+            <button className="btn" type="submit">
+              Sign Up
+            </button>
+            <p className="bottom-text">
+              By clicking the Sign Up button, you agree to our <br />
+              <a href="#">Terms & Conditions</a> and
+              <a href="#"> Privacy Policy</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
