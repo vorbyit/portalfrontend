4fb57dbbd2f9c89cea87b740271f11b5b68fa73a
Mihir <pavuskarmihir@gmail.com>
2020-03-15 13:26:21 +0530

    accept and rejection appointment added

--- src/components/ExpertApptPage.jsx
+++ src/components/ExpertApptPage.jsx
@@ -10,11 +10,14 @@ class ExpertApptPage extends Component {
 	constructor() {
 		super()
 		this.state={
+			unapproved: [],
 			upcoming : [],
 			past : [],
 		}
+		this.approveSlot=this.approveSlot.bind(this);
+		this.rejectSlot=this.rejectSlot.bind(this);
 	}
-	
+
 	async componentDidMount() {
 		try {
 			if (isEmpty(this.props.user)) {
@@ -31,24 +34,51 @@ class ExpertApptPage extends Component {
 			const { data } = await API.get('/expert/appointments');
 			const upcoming = [];
 			const past = [];
+			const unapproved = [];
 			const d = new Date();
 			const date = d.toISOString().split('T')[0]
 			const time = d.toTimeString();
 
 			for(let i=0; i<data.length; i++){
-				if (data[i].slot.Date.split('T')[0]>date 
+				if(!data[i].slot.approved) {
+					unapproved.push(data[i]);
+				}
+				else if (data[i].slot.Date.split('T')[0]>date 
 				|| (data[i].slot.Date.split('T')[0]===date 
 				&& data[i].slot.slot > time)) {
-				  upcoming.push(data[i])
+				  upcoming.push(data[i]);
 				}
 				else {
-				  past.push(data[i])
+				  past.push(data[i]);
 				}
 			}
-			this.setState({ past, upcoming })
+			this.setState({ past, upcoming, unapproved })
 		} catch (error) {
 			console.log(error);
 		}	
+	}
+
+	rejectSlot(id) {
+		const { unapproved } = this.state;
+		for(let i=0; i< unapproved.length; i++) {
+			if(unapproved[i]._id === id) {
+				delete unapproved[i];
+				return;
+			}
+		}
+	}
+
+	approveSlot(id) {
+		const { unapproved, upcoming } = this.state;
+		for(let i=0; i< unapproved.length; i++) {
+			if(unapproved[i]._id === id) {
+				this.setState({
+					upcoming: upcoming.push(unapproved[i]),
+				})
+				delete unapproved[i];
+				return;
+			}
+		}
 	}
 
 	render() {
@@ -60,15 +90,35 @@ class ExpertApptPage extends Component {
 		return (
 			<div>
 				<div>
+					<h5>Unapporved</h5>
+					{this.state.unapproved.map((appt) =>
+						<UserCard 
+							user={this.props.user}
+							rejectSlot={this.rejectSlot}
+							approveSlot={this.approveSlot}
+							appt={appt.user}
+							slot={appt.slot}
+						/>
+					)}
+				</div>
+				<div>
 					<h5>Upcoming Appointments</h5>
 					{this.state.upcoming.map((appt) => 
-						<UserCard appt={appt.user} slot={appt.slot}/>
+						<UserCard 
+							user={this.props.user}
+							appt={appt.user} 
+							slot={appt.slot}
+						/>
 					)}
 				</div>
 				<div>
 					<h5>Past Appointments</h5>
 					{this.state.past.map((appt) => 
-						<UserCard appt={appt.user} slot={appt.slot}/>
+						<UserCard 
+							appt={appt.user} 
+							user={this.props.user}
+							slot={appt.slot}
+						/>
 					)}
 				</div>
 			</div>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,8 +1,38 @@
 import React, { Component } from 'react'
+import API from '../API';
 
 export default class UserCard extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
+		this.onApprove=this.onApprove.bind(this);
+		this.onReject=this.onReject.bind(this);
+	}
+
+	async onApprove(e) {
+		try {
+			const approved = await API.post('slots/approve', {
+				id : e.target.id,
+			});
+			console.log(approved);
+			if(approved.status === 200) {
+				this.props.approveSlot(approved.data._id);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async onReject(e) {
+		try {
+			const rejected = await API.post('slots/reject', {
+				id : e.target.id,
+			});
+			if(rejected.status === 200) {
+				this.props.rejectSlot(rejected.data._id);
+			}
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
 	render() {
@@ -14,7 +44,15 @@ export default class UserCard extends Component {
 				<div>{this.props.appt.education}</div>
 				<div>{this.props.slot.Date.split('T')[0]}</div>
 				<div>{this.props.slot.slot}</div>
-				<button>Message</button>
+				{!this.props.slot.approved ? this.props.user.type !== "EXPERT" ? 
+					<div>APPROVAL PENDING</div>
+				: 
+					<div>
+						<button id={this.props.slot._id} onClick={this.onApprove}>Approve</button>
+						<button id={this.props.slot._id} onClick={this.onReject}>Reject</button>
+					</div> :
+					<button>Message</button>
+				}
 			</div>
 		)
 	}
