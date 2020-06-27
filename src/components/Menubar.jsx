e8e16af36c6fd5ee52847aa1eb6861ab72dbfc20
PavanSiddharth <pavaneasan@gmail.com>
2020-06-27 17:38:36 +0530

    Made Expert Appt page responsive

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,38 +1,60 @@
 import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
 import "../css/Menubar.css";
 
 
 class Menubar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false
+    };
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
   render() {
     return (
+
+
       <div>
-        <div className="menu">
-          <div className="txt">Thankyou for your support</div>
-          <ul>
-            <li>
-              <NavLink to="/appointments" className="a" activeClassName="active">
+        <div className="txt">Thankyou for your support</div>
+        <div>
+      <Navbar light expand="md">
+              <NavbarToggler onClick={this.toggleNav} />
+              <Collapse isOpen={this.state.isNavOpen} navbar>
+                  <Nav className="menu" navbar>
+                  <NavItem>
+                  <NavLink to="/appointments" className="a" activeClassName="active">
                 Appointments
               </NavLink>
-            </li>
-            <li>
-              <NavLink to="/addslots" className="a" activeClassName="active">
+                  </NavItem>
+                  <NavItem>
+                  <NavLink to="/addslots" className="a" activeClassName="active">
                 Time Slots
               </NavLink>
-            </li>
-            <li>
-              <NavLink to="/nolink" className="a" activeClassName="active">
+                  </NavItem>
+                  <NavItem>
+                  <NavLink to="/nolink" className="a" activeClassName="active">
                 Messaging
               </NavLink>
-            </li>
-            <li>
-              <NavLink to="/profile" className="a" activeClassName="active">
+                  </NavItem>
+                  <NavItem>
+                  <NavLink to="/profile" className="a" activeClassName="active">
                 Your Profile{" "}
               </NavLink>
-            </li>
-          </ul>
-        </div>
+                  </NavItem>
+                  </Nav>
+              </Collapse>
+      </Navbar>
       </div>
+  </div>
     );
   }
 }
