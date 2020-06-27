b0dfa1bfd3ec8d26a04e89e9210358e0e78485c3
PavanSiddharth <pavaneasan@gmail.com>
2020-06-28 01:46:19 +0530

    Conflicts merged

--- src/components/ExpandedExpertCard.jsx
+++ src/components/ExpandedExpertCard.jsx
@@ -1,3 +1,5 @@
+
+  
 import React, { Component } from "react";
 import API from "../API";
 import { withRouter } from "react-router-dom";
@@ -19,12 +21,14 @@ class ExpandedExpertCard extends Component {
           ? null
           : Object.keys(expert.slots)[0]
         : null,
-      bookSlot: undefined,
+      durationIndex: 1,
+      choosenSlot: null,
     };
 
     this.setDate = this.setDate.bind(this);
-    this.bookSlot = this.bookSlot.bind(this);
+    this.chooseSlot = this.chooseSlot.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleDuration = this.toggleDuration.bind(this);
     this.paymentHandler = this.paymentHandler.bind(this);
   }
 
@@ -35,33 +39,12 @@ class ExpandedExpertCard extends Component {
     });
   }
 
-  bookSlot(evt) {
+  chooseSlot(evt) {
     const slot = evt.target.id;
     if (this.props.expert.slots[this.state.currDate][slot] !== null) {
       console.log("BOOKED");
     } else {
-      this.setState({
-        bookSlot: slot,
-      });
-    }
-  }
-
-  async handleSubmit(evt) {
-    evt.preventDefault();
-    console.log(this.state);
-    if (this.state.showDetails === false) {
-      this.setState({
-        showDetails: true,
-      });
-    } else if (this.state.bookSlot === undefined) {
-      console.log("Select SLOT");
-    } else {
-      const slot = await API.post("/slots/bookslot", {
-        date: this.state.currDate,
-        slot: this.state.bookSlot,
-        expertId: this.props.expert._id,
-      });
-      console.log(slot);
+      this.setState({ choosenSlot: slot, });
     }
   }
 
@@ -152,13 +135,33 @@ class ExpandedExpertCard extends Component {
       rzp1.open();
     }
 
-  handlePayment(e) {
-    console.log(e.target.value);
+
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    if (this.state.choosenSlot === null) {
+      alert("Select SLOT");
+      return;
+    }
     this.props.history.push({
       pathname: "/payment",
       data: this.props.expert._id,
-      amount : e.target.value
+      date: this.state.currDate,
+      slot: this.state.choosenSlot,
+      amount: this.state.durationIndex*100
     });
+    // const slot = await API.post("/slots/bookslot", {
+    //   date: this.state.currDate,
+    //   slot: this.state.bookSlot,
+    //   expertId: this.props.expert._id,
+    // });
+  }
+
+  toggleDuration(e, btnNo) {
+    e.preventDefault();
+    if (btnNo === this.state.durationIndex) {
+      return;
+    }
+    this.setState({ durationIndex: btnNo, });
   }
 
   render() {
@@ -172,54 +175,41 @@ class ExpandedExpertCard extends Component {
               src={expert.pic === "defaultpic" ? defaultPic : expert.pic}
               alt="profpic"
             />
-
             <div>
               <h3>{expert.name}</h3>
               <h3>{expert.institution}</h3>
               <h3>{expert.branch}</h3>
               <div className="btn-container">
-                <button className="book-slot-btn" onClick={this.handleSubmit}>
-                  BOOK SLOT
+                <button className="book-slot-btn" onClick={(e) => this.handleSubmit(e)}>
+                  PROCEED TO PAY
                 </button>
               </div>
             </div>
           </div>
           <div className="details">
-            <button onClick={(e) => this.props.minimizeCard(e)}>X</button>
+            <button className="close-btn" onClick={(e) => this.props.minimizeCard(e)}>X</button>
             <div className="description">
               <h1>Description</h1>
               <p>{expert.desc}</p>
             </div>
-            <div className="form">
-              <div class="duration">
-                <h2>Select Call Duration</h2>
-                <div class="duration-input">
-                  <input
-                    type="radio"
-                    name="duration"
-                    id="30min"
-                    value="30"
-                    checked
-                  />
-                  <label for="30min">
-                    {" "}
-                    <span>30 min&nbsp;</span>{" "}
-                    <button value="100" onClick={(e) => this.paymentHandler(e)}>
+            <div class="duration">
+              <h2>Select Call Duration</h2>
+              <div class="duration-btn-container">
+                <button className={this.state.durationIndex === 1 ? "duration-btn btn-selected" : "duration-btn"} onClick={(e) => this.toggleDuration(e, 1)}>
+                  <span>30 Min</span>
+                  <span>|</span>
+                  <button value="100" onClick={(e) => this.paymentHandler(e)}>
                       100 Rs
                     </button>
-                  </label>
-
-                  <input type="radio" name="duration" id="60min" value="60" />
-                  <label for="60min">
-                    {" "}
-                    <span>60 min&nbsp;</span>{" "}
-                    <button value="200" onClick={(e) => this.paymentHandler(e)}>
+                </button>
+                <button className={this.state.durationIndex === 2 ? "duration-btn btn-selected" : "duration-btn"} onClick={(e) => this.toggleDuration(e, 2)}>
+                  <span>60 Min</span>
+                  <span>|</span>
+                  <button value="200" onClick={(e) => this.paymentHandler(e)}>
                       200 Rs
                     </button>
-                  </label>
-                </div>
+                </button>
               </div>
-              <div></div>
             </div>
             {isEmpty(expert.slots) ? null : (
               <div className="slots">
@@ -228,12 +218,13 @@ class ExpandedExpertCard extends Component {
                   {!expert.slots[this.state.currDate]
                     ? null
                     : Object.keys(expert.slots[this.state.currDate]).map(
-                        (slot) => (
-                          <label id={slot} onClick={this.bookSlot}>
-                            {slot}
-                          </label>
-                        )
-                      )}
+                      (slot) => (
+                        <label id={slot} className={this.state.choosenSlot === slot ? "slot-selected" : null} onClick={(e) => this.chooseSlot(e)}>
+                          {slot}
+                        </label>
+                      )
+                    )
+                  }
                 </div>
                 <div className="date-input">
                   {Object.keys(expert.slots).map((date) => (
